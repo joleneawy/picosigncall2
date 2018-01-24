0f3c6a7006ee7c5079c2da7c8f6786c630113a2d
jolene.aw <Aw061301281>
2018-01-24 17:09:10 +0800

    First Commit

--- src/main/java/PicoRunnerMain2.java
+++ src/main/java/PicoRunnerMain2.java
@@ -8,32 +8,29 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 @SuppressWarnings("deprecation")
-public class PicoRunnerMain {
+public class PicoRunnerMain2 {
 	public static void main(String[] args) {
 		
 		String picoid = null;
-		String meetingroomid = null;
 		
 		if(args.length==0){
 			System.exit(0);
 		}
 
-		if(args.length==2){
+		if(args.length==1){
 			picoid = args[0];
-			meetingroomid = args[1];
 		
-			if(picoid==null || picoid.equalsIgnoreCase("") || meetingroomid==null || meetingroomid.equalsIgnoreCase("")){
+			if(picoid==null || picoid.equalsIgnoreCase("")){
 				System.exit(0);
 			}
 			
 			System.out.println("picoid: "+picoid);
-			System.out.println("meetingroomid: "+meetingroomid);
 		}
 
 		@SuppressWarnings("resource")
 		HttpClient httpClient = new DefaultHttpClient();
 		try {
-			HttpGet httpGetRequest = new HttpGet("http://localhost:8080/pico/rest/generate/"+picoid+"/"+meetingroomid);
+			HttpGet httpGetRequest = new HttpGet("http://localhost:8080/PicoService/rest/generate/"+picoid);
 			HttpResponse httpResponse = httpClient.execute(httpGetRequest);
 
 			System.out.println("----------------------------------------");
